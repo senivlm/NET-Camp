31a694dcdbaabc3d8ad07bbf061db029462e0c63
oleg.simashkevych <simol7777@gmail.com>
2022-05-21 15:10:02 +0300

    HW 03 21.05

---
+++ HomeWork_03/Program.cs
@@ -0,0 +1,49 @@
+﻿using HomeWork_03;
+
+try 
+{
+    //Додати в клас Vector метод, який перевіряє, чи поле є паліндромом.
+    Vector vectorForTest1 = new(19);
+    vectorForTest1.InitRandom(1, 10);
+    bool isPalindrome = vectorForTest1.IsPalindrome();
+    Console.WriteLine($"Vector: {vectorForTest1}");
+    Console.WriteLine(isPalindrome ? "It is a Palindrome" : "It isn't a Palindrome");
+    Console.WriteLine();
+
+    //Додати в клас Vector метод, який реверсує елементи масиву. Створити власний метод. А також показати використання стандартного методу.
+    Vector vectorForTest2 = new(17);
+    vectorForTest2.InitRandom(1, 10);
+    Console.WriteLine($"Original           Vector: {vectorForTest2}");
+    vectorForTest2.Reverse(ImplementationMethod.own);
+    Console.WriteLine($"Reversed           Vector: {vectorForTest2}");
+    vectorForTest2.Reverse(ImplementationMethod.standart);
+    Console.WriteLine($"Return to original Vector: {vectorForTest2}");
+    Console.WriteLine();
+
+    //Додати в клас Vector метод, який в масиві знаходить найдовшу підпослідовність однакових чисел.
+    Vector vectorForTest3 = new(22);
+    vectorForTest3.InitRandom(1, 4);
+    Console.WriteLine($"Vector: {vectorForTest3}");
+    Pair? LongestSubSequence = vectorForTest3.GetLongestSubSequence();
+    Console.WriteLine($"First Longest SubSequence is {LongestSubSequence}");
+    Console.WriteLine();
+
+    //У класі Matrix створити метод, який заповнює квадратну матрицю діагональною змійкою,
+    //параметром методу має бути напрям початкового повороту змійки (вправо, чи вниз), заданий змінною типу Enum.
+    Matrix matrix4 = new(4);
+    matrix4.InitDiagonalSnake(Direction.right);
+    Console.WriteLine($"Matrix right: \n{matrix4}");
+    Console.WriteLine();
+    
+    Matrix matrix5 = new(5);
+    matrix5.InitDiagonalSnake(Direction.down);
+    Console.WriteLine($"Matrix down: \n{matrix5}");
+    Console.WriteLine();
+
+    //Оптимізувати метод InitShufle класу Vector, створений на занятті.
+
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}
---
+++ HomeWork_03/Vector.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_03
+{
+    internal class Vector
+    {
+        private readonly int[] array;
+
+        public int Lenght => array.Length;
+
+        public Vector(uint n)
+        {
+            if (n == 0)
+            {
+                throw new ArgumentException("Vector size is 0");
+            }
+            array = new int[n];
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                if (index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                return array[index];
+            }
+            set
+            {
+                if (index >= array.Length)
+                {
+                    throw new IndexOutOfRangeException();
+                }
+                array[index] = value;
+            }
+
+
+        }
+
+        public void InitRandom(int a, int b)
+        {
+            Random ran = new();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = ran.Next(a, b);
+            }
+        }
+
+        public void InitShuffle()
+        {
+            Random ran = new();
+            for (int i = 0; i < array.Length; i++)
+            {
+                while (true)
+                {
+                    int nom = ran.Next(1, array.Length + 1);
+                    bool isExist = false;
+                    for (int j = 0; j < i; j++)
+                    {
+                        if (array[j] == nom)
+                        {
+                            isExist = true;
+                            break;
+                        }
+                    }
+                    if (!isExist)
+                    {
+                        array[i] = nom;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        public bool IsPalindrome()
+        {
+            bool result = true;
+            for (int i = 0; i < array.Length/2 ; i++)
+            {
+                if (array[i] != array[array.Length - i - 1])
+                {
+                    result = false;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public void Reverse(ImplementationMethod method = ImplementationMethod.standart)
+        {
+            if (method == ImplementationMethod.standart)
+            {
+                Array.Reverse(array, 0, array.Length);
+            }
+            else
+            {
+                for (int i = 0; i < array.Length / 2; i++)
+                {
+                    int mirrorIndex = array.Length - i - 1;
+                    (array[i], array[mirrorIndex]) = (array[mirrorIndex], array[i]);
+                }
+            }
+        }
+
+        public Pair[] CalculateFreq()
+        {
+            int countLenght = 0;
+            Pair[] pairs = new Pair[array.Length];
+
+            //Objects pair are created on first use
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                bool isElement = false;
+                for (int j = 0; j < countLenght; j++)
+                {
+                    if (array[i] == pairs[j].Number)
+                    {
+                        pairs[j].Freq++;
+                        isElement = true;
+                        break;
+                    }
+
+                }
+                if (!isElement)
+                {
+                    pairs[countLenght] = new(array[i], 1);
+                    countLenght++;
+                }
+            }
+
+            Array.Resize(ref pairs, countLenght);
+
+            return pairs;
+        }
+
+        public Pair[] CalculateSubSequences()
+        {
+
+            int countLenght = 0;
+            Pair[] pairs = new Pair[array.Length];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (pairs[countLenght] == null)
+                {
+                    pairs[countLenght] = new(array[i], 1);
+                }
+                else if (pairs[countLenght].Number != array[i])
+                {
+                    pairs[++countLenght] = new(array[i], 1);
+                }
+                else
+                {
+                    pairs[countLenght].Freq++;
+                }
+            }
+
+            Array.Resize(ref pairs, countLenght);
+
+            return pairs;
+
+        }
+
+        public Pair? GetLongestSubSequence()
+        {
+            
+            Pair[] pairs = CalculateSubSequences();
+
+            if (pairs.Length == 0)
+            {
+                return null;
+            }
+            
+            Pair resultPair = pairs[0];
+            
+            for (int i = 1; i < pairs.Length; i++)
+            {
+                if (pairs[i].Freq > resultPair.Freq)
+                {
+                    resultPair = pairs[i];
+                }
+
+            }
+
+            return resultPair;
+
+        }
+
+        public override string? ToString()
+        {
+            string str = "";
+            for (int i = 0; i < array.Length; i++)
+            {
+                str += array[i] + " ";
+            }
+
+            return str;
+        }
+
+        public override bool Equals(object? obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+    }
+}
