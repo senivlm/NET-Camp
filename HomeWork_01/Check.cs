8a595895b0bad6985215f2016c92741f0b6b6083
senivlm <91743906+senivlm@users.noreply.github.com>
2022-05-24 17:18:45 +0300

    Update Check.cs

--- HomeWork_01/Check.cs
+++ HomeWork_01/Check.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace NET_CAMP_HomeWork_s1_01
 {
     public static class Check
-    {
+    {//краще на вході тільки покупку. Групу товарів окремого покупця
         public static void ShowAbout(Product product)
         {
             Console.WriteLine($"Product {product.Name} Price {product.Price} TotalWeight {product.Weight}");
