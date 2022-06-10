baa5b450eccbcfaabbc47db49808cb36876d9981
oleg.simashkevych <simol7777@gmail.com>
2022-06-10 19:20:26 +0300

    ClassWork

---
+++ _String/Program.cs
@@ -0,0 +1,26 @@
+﻿using _String;
+
+//string str = "qwerty asdfgh   zxcvbn";
+
+//Console.WriteLine(str.ToUpper());
+
+//var sb = new System.Text.StringBuilder();
+//sb.Append(Char.ToUpper(str[0]));
+//sb.Append(str.Substring(1));
+////sb.Append(str[1..]);
+
+//string str2 = sb.ToString();
+//Console.WriteLine(str2);
+
+//string[] wordArray = str.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+//foreach (string word in wordArray)
+//{
+//    Console.WriteLine(word);
+//}
+
+
+
+//List<(string, int, double)> list = new List<(string, int, double)>();
+//list.Add(ReaderStr.Parse("Asadasdsd  12  2,3"));
+//list.Add(ReaderStr.Parse("sdfdfdfdf  1,2  2,
+
---
+++ _String/ReaderStr.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _String
+{
+    internal class ReaderStr
+    {
+
+
+        public static (string, int, double) Parse(string str)
+        {
+            string[] data = str.Split(" ");
+
+            return (data[0], int.Parse(data[1]), double.Parse(data[2]));
+        }
+
+    }
+}
