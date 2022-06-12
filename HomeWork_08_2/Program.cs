2a5cb9597f193c3f395c12069cf0e5bf9110b3c0
oleg.simashkevych <simol7777@gmail.com>
2022-06-12 15:46:42 +0300

    update 12.06

--- HomeWork_08_2/Program.cs
+++ HomeWork_08_2/Program.cs
@@ -1,9 +1,21 @@
 ﻿using HomeWork_08_2;
 
+//Дано текстовий файл зі статистикою відвідування сайту за тиждень. Кожен рядок містить ip адресу, час і назву дня тижня. 
+//(наприклад, 139.18.150.126 23:12:44 sunday).
+//Для кожного ip вкажіть кількість відвідувань на тиждень, найбільш популярний день тижня,
+//найбільш популярний відрізок часу довжиною в одну годину.
+//Знайдіть також найбільш популярний відрізок часу в добі довжиною одну годину в цілому для сайту.
+//Продумайте, як оптимально здійснити повторювану дію для різних даних.
+
+
+//Файл з даними дляаналізу
 string fileForAnaliz = "..\\..\\..\\Connect.log";
 
-//Виклик одноразово
-//GeneratorOfLogs.Create(fileForAnaliz, 10000);
+//Виклик одноразово для створення файлу
+if (!File.Exists(fileForAnaliz))
+{
+    GeneratorOfLogs.Create(fileForAnaliz, 10000);
+}
 
 //Логгер помилок
 Logger loggerError = new("..\\..\\..\\LogError.log");
@@ -24,10 +36,10 @@ logAnalizer.ExtDisplayAction += Dispay.Add; //дублюємо у файл
 logAnalizer.Load(fileForAnaliz);
 
 //Розділимо по IP
-var dicSplitByIP = logAnalizer.SplitByField((LogRecord record) => record.IPAdress);
+Dictionary<object, LogAnalyzer> dicSplitByIP = logAnalizer.SplitByField((LogRecord record) => record.IPAdress);
 
 //Статистика по IP
-foreach (var recDic in dicSplitByIP)
+foreach (KeyValuePair<object, LogAnalyzer> recDic in dicSplitByIP)
 {
     LogAnalyzer logAnalizerIP = recDic.Value;
     logAnalizerIP.ShowStat(recDic.Key.ToString()??"");
