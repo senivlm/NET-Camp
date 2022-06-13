0504591d468d9485e81e0c592728bca2ab31f48d
oleg.simashkevych <simol7777@gmail.com>
2022-06-13 13:34:21 +0300

    Add HomeWork_08_3

---
+++ HomeWork_08_3/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_08_3
+{
+    public class Product : IEquatable<Product>
+    {
+        //Fields
+        private float price;
+        private float weight;
+
+        //Properties
+        public string Name { get; } = ""; //так як string може зберыгати null, по контекту задачі нам він не треба.
+        public float Price { get => this.price; set { this.price = (value >= 0) ? value : 0; } }
+        public float Weight { get => this.weight; set { this.weight = (value >= 0) ? value : 0; } }
+
+        //Constructors
+        //Свідомо не робив конструктор без параметрів, 3oоб захиститісь від створення об'ектів без ініціализованих обов`язкових полів\властивостей. Відкритий до дискусії стосовно цього.
+        public Product(string name) => this.Name = name;
+        public Product(string name, float price, float weight) : this(name)
+        {
+            this.Price = price;
+            this.Weight = weight;
+        }
+
+        //Methods
+        public override string? ToString()
+        {
+            return String.Format($"Product name={this.Name} price={this.Price} weight={this.Weight}");
+        }
+        public override int GetHashCode() => (Name, Price, Weight).GetHashCode();
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as Product);
+        }
+        public bool Equals(Product? other) //IEquatable<Product>
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            return this.Name.Equals(other.Name)
+                && this.price == other.price
+                && this.weight == other.weight
+                ;
+        }
+        public virtual void SetPrice(float percent)
+        {
+            this.Price = this.Price * (1 + percent / 100);
+        }
+
+    }
+}
---
+++ HomeWork_08_3/Program.cs
@@ -0,0 +1,55 @@
+﻿//Порівняти 2 об’єкти класу Склад і визначити наступні результати:
+//1. Товари є в першому складі і немає в другому.
+//2. Товари, які  є спільними в обох складах.
+//3. Спільний список товарів, які є на обох складах, без повторів елементів.
+
+using HomeWork_08_3;
+
+//Логгер помилок
+Logger loggerError = new("..\\..\\..\\LogError.log");
+loggerError.ExtDisplayAction = Console.WriteLine;
+
+//Логгер успіху
+Logger loggerSuccess = new("..\\..\\..\\LogSuccess.log");
+loggerSuccess.ExtDisplayAction = Console.WriteLine;
+
+//для перенаправлення виводу результату
+Logger Dispay = new("..\\..\\..\\DisplayResult.log");
+Dispay.ExtDisplayAction = Console.WriteLine;
+
+//Сховище 1
+Storage storageA = new();
+storageA.ExtDisplayAction = Console.WriteLine;  //виводимо результат у консоль
+storageA.ExtDisplayAction += Dispay.Add; //дублюємо у файл
+//storageA.ExtInputAction = Console.ReadLine;
+storageA.LoggerErrorAdd += loggerError.Add;
+storageA.LoggerSuccessAdd += loggerSuccess.Add;
+//Читання з файлу
+storageA.ReadProductsFromFile("..\\..\\..\\", "Input1.txt");
+
+//Сховище 2
+Storage storageB = new();
+storageB.ExtDisplayAction = Console.WriteLine;
+storageB.ExtInputAction = Console.ReadLine;
+storageB.LoggerErrorAdd += loggerError.Add;
+storageB.LoggerSuccessAdd += loggerSuccess.Add;
+//Читання з файлу
+storageB.ReadProductsFromFile("..\\..\\..\\", "Input2.txt");
+
+storageA.ShowAll("storageA");
+storageB.ShowAll("storageB");
+
+//1. Товари є в першому складі і немає в другому.
+Storage storage1 = storageA.Except(storageB);
+storage1.ShowAll("storageA.Except(storageB)");
+
+//2. Товари, які  є спільними в обох складах.
+Storage storage2 = storageA.Intersect(storageB);
+storage2.ShowAll("storageA.Intersect(storageB)");
+
+//3. Спільний список товарів, які є на обох складах, без повторів елементів.
+Storage storage3 = storageA & storageB;
+storage3.ShowAll("storage3 = storageA & storageB");
+
+
+
