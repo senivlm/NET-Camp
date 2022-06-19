9cd8e3268847c82e1b124eb7b68fe1ce195e558e
oleg.simashkevych <simol7777@gmail.com>
2022-06-19 13:58:45 +0300

    update 19.06

--- HomeWork_06_2/StringReaderCamp.cs
+++ HomeWork_06_2/StringReaderCamp.cs
@@ -59,7 +59,7 @@ namespace HomeWork_06_2
             {
                 using (StreamReader sr = new StreamReader(FileName))
                 { 
-                    string currentLine = "";
+                    string currentSentence = "";
                     while (!sr.EndOfStream)
                     {
                         string? lastLineRead = sr.ReadLine();
@@ -69,7 +69,7 @@ namespace HomeWork_06_2
 
                             //У currentLine в минулої ітерації циклу прийходить початок незавершенного речення.
                             //до нього додаємо  subLine[0] як його продовження
-                            currentLine = (string.IsNullOrEmpty(currentLine)) ? subLine[0] : $"{currentLine} {subLine[0]}";
+                            currentSentence = string.IsNullOrEmpty(currentSentence) ? subLine[0] : $"{currentSentence} {subLine[0]}";
 
                             //Якшо subLine має больше 1 єлементу, то в прочинаному lastLineRead є кінець речення и можно його записати у result 
                             if (subLine.Length > 1)
@@ -77,17 +77,17 @@ namespace HomeWork_06_2
                                 //Нульовий у currentLine, а останній обходимо, а ле у файл не пишемо, а запам'ятовуемо у currentLine
                                 for (int i = 1; i < subLine.Length; i++)
                                 {
-                                    result.Add(currentLine);
+                                    result.Add(currentSentence);
                                     //У currentLine кладемо початок наступного речення
-                                    currentLine = subLine[i];
+                                    currentSentence = subLine[i];
                                 }
                             }
                         }
                     }
                     //Якшщо у currentLine шось залишилось, то вважаемо це окремим реченням яке було без крапки и записуємо його
-                    if (!string.IsNullOrEmpty(currentLine)) 
+                    if (!string.IsNullOrEmpty(currentSentence)) 
                     {
-                        result.Add(currentLine);
+                        result.Add(currentSentence);
                     }
 
                 }
---
+++ _Collection/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+Dictionary<string, int> dic = new();
+dic.Add("sdfsd", 23);
+dic.Add("sasdsad", 23);
+
+
+int d = 1;
