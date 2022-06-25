05a45054f12da4987d6b865cfbd325779d4cd8f2
oleg.simashkevych <simol7777@gmail.com>
2022-06-25 16:21:54 +0300

    update 25/06

--- _Math/Matrix.cs
+++ _Math/Matrix.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Math
 {
-    public class Matrix
+    public class Matrix : IEnumerable<int>
     {
         #region fields
         private int[,] matrix;
@@ -50,16 +51,10 @@ namespace Math
         #endregion
 
         #region init_methods
-        public void InitRandom(int a, int b)
+        public void InitHorizontalSnake()
         {
-            Random ran = new();
-            for (int x = 0; x < LenghtX; x++)
-            {
-                for (int y = 0; y < LenghtY; y++)
-                {
-                    matrix[x, y] = ran.Next(a, b);
-                }
-            }
+            List<(int, int)> indexes = GetIndexesHorizontalSnake();
+            InitByIndexes(indexes);
         }
         public void InitDiagonalSnake(Direction direction)
         {
@@ -67,53 +62,26 @@ namespace Math
             {
                 throw new ArgumentException("Matrix isn't square");
             }
-
-            int value = 1;
-            int mirrorValue = Lenght; // equal (Lenght - value + 1) 
-
-            for (int i = 0; i < LenghtX; i++)
-            {
-                for (int j = 0; j <= i; j++)
-                {
-                    int x = i - j; 
-                    int y = j;
-
-                    if (
-                        (i%2!=0 && direction == Direction.RIGHT)
-                        || (i%2==0 && direction == Direction.DOWN)
-                        )
-                    {
-                        (x, y) = (y, x);
-                    }
-
-                    int mirrorX = LenghtX - x - 1;
-                    int mirrorY = LenghtY - y - 1;
-
-                    this.matrix[x, y] = value++;
-                    this.matrix[mirrorX, mirrorY] = mirrorValue--;
-
-                    if (value > mirrorValue)
-                    {
-                        break;
-                    }
-                }
-
-                if (value > mirrorValue)
-                {
-                    break;
-                }
-
-            }
-
+            List<(int, int)> indexes = GetIndexesDiagonalSnake(direction);
+            InitByIndexes(indexes);
         }
-        public void InitFronStream(StreamReader stream)
+        public void InitRandom(int a, int b)
+        {
+            List<(int, int)> indexes = GetIndexesLine();
+            Random ran = new();
+            foreach ((int x, int y) in indexes)
+            {
+                matrix[x, y] = ran.Next(a, b);
+            }
+        }
+        public void InitFromStream(StreamReader stream)
         {
             string? line = stream.ReadLine();
             if (line == null)
             {
                 throw new IOException("Incorrect format file (empry)");
             }
-            char[] separators = {' ', "\t"[0]};
+            char[] separators = { ' ', "\t"[0] };
             string[] size = line.Split(separators, StringSplitOptions.RemoveEmptyEntries);
             if (size.Length != 2)
             {
@@ -139,7 +107,7 @@ namespace Math
 
                 for (int j = 0; j < coloms; j++)
                 {
-                    this.matrix[i,j] = int.Parse(value[j]);
+                    this.matrix[i, j] = int.Parse(value[j]);
                 }
             }
 
@@ -148,16 +116,149 @@ namespace Math
                 throw new IOException("Incorrect format file (many lines)");
             }
         }
+
+        private List<(int, int)> GetIndexesLine()
+        {
+            List<(int, int)> result = new();
+
+            for (int x = 0; x < LenghtX; x++)
+            {
+                for (int y = 0; y < LenghtY; y++)
+                {
+                    result.Add((x, y));
+                }
+            }
+            return result;
+        }
+        private List<(int, int)> GetIndexesHorizontalSnake()
+        {
+            List<(int, int)> result = new();
+
+            for (int x = 0; x < LenghtX; x++)
+            {
+                for (int yTmp = 0; yTmp < LenghtY; yTmp++)
+                {
+                    int y = (x % 2 == 0) ? yTmp : (LenghtY - yTmp - 1);
+                    result.Add((x, y));
+                }
+            }
+            return result;
+        }
+        private List<(int, int)> GetIndexesDiagonalSnake(Direction direction)
+        {
+            if (LenghtX != LenghtY)
+            {
+                throw new ArgumentException("Matrix isn't square");
+            }
+
+            List<(int, int)> result = new();
+            for (int i = 0; i <= (LenghtX - 1) * 2; i++)
+            {
+                int jStart;
+                int jFinish;
+
+                if (i < LenghtX)
+                {
+                    //Перша половина
+                    jStart = 0;
+                    jFinish = i;
+                }
+                else
+                {
+                    //Друга половина
+                    jStart = i - LenghtX + 1;
+                    jFinish = (LenghtX - 1);
+                }
+
+                for (int j = jStart; j <= jFinish; j++)
+                {
+                    int x = i - j;
+                    int y = j;
+
+                    if (
+                        (i % 2 != 0 && direction == Direction.RIGHT)
+                        || (i % 2 == 0 && direction == Direction.DOWN)
+                        )
+                    {
+                        (x, y) = (y, x);
+                    }
+                    result.Add((x, y));
+                }
+            }
+            return result;
+        }
+        private void InitByIndexes(List<(int, int)> indexes)
+        {
+            int value = 1;
+            foreach ((int x, int y) in indexes)
+            {
+                matrix[x, y] = value++;
+            }
+        }
+        #endregion
+
+        #region Enumerators
+        public IEnumerator<int> GetEnumeratorHorizontalSnake()
+        {
+            List<(int, int)> indexes = GetIndexesHorizontalSnake();
+            foreach ((int x, int y) in indexes)
+            {
+                yield return matrix[x, y];
+            }
+        }
+        public IEnumerator<int> GetEnumeratorDiagonalSnake(Direction direction)
+        {
+            if (LenghtX != LenghtY)
+            {
+                throw new ArgumentException("Matrix isn't square");
+            }
+
+            List<(int, int)> indexes = GetIndexesDiagonalSnake(direction);
+            foreach ((int x, int y) in indexes)
+            {
+                yield return matrix[x, y];
+            }
+        }
+        public IEnumerator<int> GetEnumerator()
+        {
+            return GetEnumeratorDiagonalSnake(Direction.RIGHT);
+        }
+        public IEnumerable<int> GetEnumerableHorizontalSnake()
+        {
+            List<(int, int)> indexes = GetIndexesHorizontalSnake();
+            foreach ((int x, int y) in indexes)
+            {
+                yield return matrix[x, y];
+            }
+        }
+        public IEnumerable<int> GetEnumerableDiagonalSnake(Direction direction)
+        {
+            if (LenghtX != LenghtY)
+            {
+                throw new ArgumentException("Matrix isn't square");
+            }
+
+            List<(int, int)> indexes = GetIndexesDiagonalSnake(direction);
+            foreach ((int x, int y) in indexes)
+            {
+                yield return matrix[x, y];
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
         #endregion
 
         #region other_methods
         public void SaveToStream(StreamWriter stream)
         {
             stream.WriteLine($"{this.LenghtX} {this.LenghtY}");
-            stream.WriteLine(this.ToString()); 
+            stream.WriteLine(this.ToString());
 
         }
         #endregion
 
     }
 }
+
